52186b26d76223b89362ba910e19f4e25bbc1faa
ihsan <zainul.ariffinihsan@gmail.com>
2026-07-26 15:45:01 +0700

    Fix database SSL CA path for Render

--- config/database.php
+++ config/database.php
@@ -59,7 +59,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') 
+                    ? (file_exists(env('MYSQL_ATTR_SSL_CA')) ? env('MYSQL_ATTR_SSL_CA') : storage_path('certs/aiven-ca.pem'))
+                    : null,
             ]) : [],
         ],
 
@@ -79,7 +81,9 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA') 
+                    ? (file_exists(env('MYSQL_ATTR_SSL_CA')) ? env('MYSQL_ATTR_SSL_CA') : storage_path('certs/aiven-ca.pem'))
+                    : null,
             ]) : [],
         ],
 
