7870ec222465b402c999d168a8a1e85f0041b27e
ihsan <zainul.ariffinihsan@gmail.com>
2026-07-26 15:26:20 +0700

    tes2

--- app/Mail/OtpMail.php
+++ app/Mail/OtpMail.php
@@ -6,8 +6,9 @@ use Illuminate\Mail\Mailable;
 use Illuminate\Mail\Mailables\Content;
 use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class OtpMail extends Mailable
+class OtpMail extends Mailable implements ShouldQueue
 {
     use SerializesModels;
 
