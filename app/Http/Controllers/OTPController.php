d165e337c982f36f315fdefc43810ff45445cfd8
ihsan <zainul.ariffinihsan@gmail.com>
2026-07-27 23:40:17 +0700

    sss

--- app/Http/Controllers/OTPController.php
+++ app/Http/Controllers/OTPController.php
@@ -167,7 +167,7 @@ class OTPController extends Controller
         
         OtpVerification::create([
             'phone_number' => $phone,
-            'otp_hash' => Hash::driver('bcrypt')->make($otp),
+            'otp_hash' => Hash::make($otp),
             'expired_at' => now()->addMinutes(15),
             'attempts' => 0,
         ]);
